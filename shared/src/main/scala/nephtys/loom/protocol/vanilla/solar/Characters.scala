48f7342591c58a7e3ac75b81c7f0936bffd4a419
nephtys <christopher.kaag@gmail.com>
2016-12-09 21:02:20 +0100

    enabled creation and listing of solars

--- shared/src/main/scala/nephtys/loom/protocol/vanilla/solar/Characters.scala
+++ shared/src/main/scala/nephtys/loom/protocol/vanilla/solar/Characters.scala
@@ -1,5 +1,6 @@
 package nephtys.loom.protocol.vanilla.solar
 
+import nephtys.loom.protocol.vanilla.solar.Misc._
 import org.nephtys.loom.generic.protocol.InternalStructures.{Email, ID}
 
 import scala.scalajs.js.annotation.JSExportAll
@@ -10,5 +11,8 @@ import scala.scalajs.js.annotation.JSExportAll
 @JSExportAll
 object Characters {
 
-  def emptySolar(id : ID[Solar], owner : Email) : Solar = ???
+  def emptySolar(id : ID[Solar], owner : Email) : Solar = Solar(
+    owner, id, stillInCharGen = true, Name(""), Concept(""),Anima(""), Player(""),
+    LimitTrigger(""), Essence(1), None, WillpowerDots(5), Attributes.emptyAttributeBlock
+  )
 }
